ecc4df577dab083b2913b824abe09cc3a66c783f
charyna-chab <charyna.chab@student.passerellesnumeriques.org>
2025-01-15 12:53:19 +0700

    #2 added types of subject

---
+++ JS/customize.js
@@ -0,0 +1,22 @@
+const playQuiz = document.getElementById('play-quiz')
+const subjects = document.getElementById('types-subject')
+const content = document.querySelector('#content-1')
+const titleCategory = document.querySelector('#title-category')
+const button = document.querySelector('#btn');
+
+playQuiz.addEventListener('click',  (e) =>{
+    subjects.style.display = "block";
+    content.style.display = "none";
+    titleCategory.style.display = "none";
+})
+
+button.addEventListener('click',(e) =>{
+    subjects.style.display = "none";
+    content.style.display = "flex";
+    titleCategory.style.display = "block";
+})
+
+
+
+
+
