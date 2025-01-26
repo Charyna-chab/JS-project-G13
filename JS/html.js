30493c5579612263c3604da1769f4effad74af12
charyna-chab <charyna.chab@student.passerellesnumeriques.org>
2025-01-26 13:00:52 +0700

    #9 change link in html.js and remove mix level

--- JS/html.js
+++ JS/html.js
@@ -23,9 +23,9 @@ function startQuiz() {
 
     let difficulty = document.getElementById("difficulty").value;
     let urls = {
-        easy: "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple&encode=url3986",
-        medium: "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple&encode=url3986",
-        hard: "https://opentdb.com/api.php?amount=10&category=21&difficulty=hard&type=multiple&encode=url3986",
+        easy: "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple",
+        medium: "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple",
+        hard: "https://opentdb.com/api.php?amount=10&category=18&difficulty=hard&type=multiple",
         
     };
 
