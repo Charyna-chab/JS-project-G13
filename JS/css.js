158fa21333e0a3eb646f973d125451205d936582
charyna-chab <charyna.chab@student.passerellesnumeriques.org>
2025-01-26 13:08:09 +0700

    #8 change link API and remove mix level

--- JS/css.js
+++ JS/css.js
@@ -23,9 +23,9 @@ function startQuiz() {
 
     let difficulty = document.getElementById("difficulty").value;
     let urls = {
-        easy: "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple&encode=url3986",
-        medium: "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple&encode=url3986",
-        hard: "https://opentdb.com/api.php?amount=10&category=21&difficulty=hard&type=multiple&encode=url3986",
+        easy: "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple",
+        medium: "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple",
+        hard: "https://opentdb.com/api.php?amount=10&category=18&difficulty=hard&type=multiple",
         
     };
 
@@ -85,8 +85,7 @@ function displayQuestion() {
             <button id="submit-btn"" onclick="submitQuiz()" ${currentQuestionIndex === questions.length - 1 ? "" : "style='display:none;'"}>Submit</button>
         </div>
     `;
-
-    quizContainer.innerHTML += buttonHTML;
+        quizContainer.innerHTML += buttonHTML;
 
     timer = setInterval(() => {
         timeLeft--;
